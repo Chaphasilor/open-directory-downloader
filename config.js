69daadc097d9cb2baf94e110745eff0e386f1e87
Chaphasilor <ppp.u@web.de>
2021-02-15 21:26:22 +0100

    bump to ODD 1.9.3.8
    - fixed permission issue on linux
    - improved install experience
    - automatically create output dir if it doesn't exist yet
    - increased maintainability

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-module.exports = {
-  GitHubReleasesUrl: `https://api.github.com/repos/KoalaBear84/OpenDirectoryDownloader/releases`,
-  OpenDirectoryDownloaderVersion: {
-    version: `v1.9.3.6`,
-    releaseId: `36853483`
-  },
-  OpenDirectoryDownloaderFolder: `${__dirname}/ODD`,
-  OpenDirectoryDownloaderOutputFolder: `${__dirname}/ODD/Scans`,
-  OpenDirectoryDownloaderPath: `${__dirname}/ODD/OpenDirectoryDownloader${process.platform === `win32` ? `.exe` : ``}`,
+module.exports = {}
+
+module.exports.GitHubReleasesUrl = `https://api.github.com/repos/KoalaBear84/OpenDirectoryDownloader/releases`
+module.exports.OpenDirectoryDownloaderVersion = {
+  version: `v1.9.3.8`,
+  releaseId: `37128271`
 }
+module.exports.OpenDirectoryDownloaderFolder = `${__dirname}/ODD`
+module.exports.OpenDirectoryDownloaderOutputFolderName = `Scans`
+module.exports.OpenDirectoryDownloaderOutputFolder = `${module.exports.OpenDirectoryDownloaderFolder}/${module.exports.OpenDirectoryDownloaderOutputFolderName}`
+module.exports.OpenDirectoryDownloaderPath = `${module.exports.OpenDirectoryDownloaderFolder}/OpenDirectoryDownloader${process.platform === `win32` ? `.exe` : ``}`
