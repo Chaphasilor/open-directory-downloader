200ca656a30b87d280d4a96786d3b731359bce56
Chaphasilor <ppp.u@web.de>
2021-03-09 23:31:45 +0100

    more scan options
    - support most command line flags of ODD
    - improved error handling
    - bump version to 3.1.0

--- open-directory-downloader.js
+++ open-directory-downloader.js
@@ -29,26 +29,71 @@ module.exports.OpenDirectoryDownloader = class OpenDirectoryDownloader {
    * @param {Boolean} [options.keepUrlFile=false] Keep the text file created by the OpenDirectoryDownloader binary after the scan is done?
    * @param {Boolean} [options.performSpeedtest=false] Perform a speed test after the scan is done? (usually takes a few seconds)
    * @param {Boolean} [options.uploadUrlFile=false] Automatically upload the file containing all the found URLs to GoFile?
+   * @param {Boolean} [options.fastScan=false] Only perform actions that are fast, so no HEAD requests, etc. Might result in missing file sizes
+   * @param {Boolean} [options.exactSizes=false] Use HEAD requests to retrieve exact file sizes
+   * @param {String} [options.userAgent=???] Use a custom user agent for all HTTP requests
+   * @param {Object} [options.auth] Used to configure (HTTP Basic) auth settings
+   * @param {String} [options.auth.username=""] The user name to use for authentication
+   * @param {String} [options.auth.password=""] The password to use for authentication
+   * @param {Number} [options.threads=5] Number of threads to use for scanning
+   * @param {Number} [options.timeout=100] Number of seconds to wait before timing out
    */
   scanUrl(url, options = {}) {
     return new Promise((resolve, reject) => {
 
+      if (!url || url.length === 0) {
+        return reject([new ODDWrapperError(`Missing URL!`)])
+      }
+      
       options.keepJsonFile = options.keepJsonFile || false
       options.keepUrlFile = options.keepUrlFile || false
       options.performSpeedtest = options.performSpeedtest || false
       options.uploadUrlFile = options.uploadUrlFile || false
+      options.fastScan = options.fastScan || false
+      options.exactSizes = options.exactSizes || false
     
       // both String and URL implement the toString() method, so just use that instead of detecting the type
       let processArgs = [`-u "${url}"`, `--quit`, `--json`, ]
-      if (options.performSpeedtest) {
+      if (options.uploadUrlFile) {
         processArgs.push(`--upload-urls`)
       }
       if (options.performSpeedtest) {
         processArgs.push(`--speedtest`)
       }
+      if (options.fastScan) {
+        processArgs.push(`--fast-scan`)
+        //TODO Output indication in ODD if file sizes are not being retrieved
+      }
+      if (options.exactSizes) {
+        processArgs.push(`--exact-file-sizes`)
+      }
+      if (options.threads) {
+        processArgs.push(`--threads`)
+        processArgs.push(String(options.threads))
+      }
+      if (options.timeout) {
+        processArgs.push(`--timeout`)
+        processArgs.push(String(options.timeout))
+      }
       if (options.outputFile && options.outputFile.length > 0) {
         processArgs.push(`--output-file`)
-        processArgs.push(options.outputFile)
+        processArgs.push(`"${options.outputFile}"`)
+      }
+      // breaks parsing, and fs is used anyway to load the scan results...
+      // if (!(options.outputFile && options.outputFile.length > 0) || options.uploadUrlFile) {
+      //   processArgs.push(`--no-urls`)
+      // }
+      if (options.userAgent && options.userAgent.length > 0) { //TODO fix bug in ODD to allow for empty user agent
+        processArgs.push(`--user-agent`)
+        processArgs.push(`"${options.userAgent}"`)
+      }
+      if (options.auth?.username) {
+        processArgs.push(`--username`)
+        processArgs.push(`"${options.auth.username}"`)
+      }
+      if (options.auth?.password) {
+        processArgs.push(`--password`)
+        processArgs.push(`"${options.auth.password}"`)
       }
 
       const oddProcess = exec(`${this.executable} ${processArgs.join(` `)}`, {
@@ -74,7 +119,7 @@ module.exports.OpenDirectoryDownloader = class OpenDirectoryDownloader {
 
       oddProcess.on(`error`, (err) => {
         // console.error(`error:`, err)
-        return reject([err]);
+        return reject([new ODDWrapperError(err.message)]);
       })
       
       oddProcess.on('close', (code) => {
@@ -97,6 +142,10 @@ module.exports.OpenDirectoryDownloader = class OpenDirectoryDownloader {
         // const finalResults = output.split(`Finished indexing`)[1];
         const finalResults = output.split(`Saving URL list to file..`)[1];
 
+        if (!finalResults) {
+          return reject([new ODDWrapperError(`Failed to parse ODD output!`)])
+        }
+
         // console.log(`finalResults:`, finalResults);
         
         const redditOutputStartString = `|`;
@@ -104,6 +153,8 @@ module.exports.OpenDirectoryDownloader = class OpenDirectoryDownloader {
         const credits = `^(Created by [KoalaBear84's OpenDirectory Indexer](https://github.com/KoalaBear84/OpenDirectoryDownloader/))`;
         
         let redditOutput = `${redditOutputStartString}${finalResults.split(redditOutputStartString).slice(1).join(redditOutputStartString)}`.split(redditOutputEndString).slice(0, -1).join(redditOutputEndString);
+
+        let missingFileSizes = redditOutput.includes(`**Total:** n/a`)
 
         let sessionRegexResults = finalResults.match(/Saved\ session:\ (.*)/);
         if (!sessionRegexResults || sessionRegexResults.length <= 1) {
@@ -151,6 +202,7 @@ module.exports.OpenDirectoryDownloader = class OpenDirectoryDownloader {
               urlFile: options.keepUrlFile ? urlFile :  undefined,
               reddit: redditOutput,
               credits,
+              missingFileSizes,
             },
           ])
             
@@ -163,6 +215,7 @@ module.exports.OpenDirectoryDownloader = class OpenDirectoryDownloader {
           urlFile: options.keepUrlFile ? urlFile :  undefined,
           reddit: redditOutput,
           credits,
+          missingFileSizes,
         })
         
       });
