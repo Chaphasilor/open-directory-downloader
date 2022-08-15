d478c8ac47b2d8ddcebb86fcc2248b8eef985820
Chaphasilor <ppp.u@web.de>
2022-08-15 12:44:37 +0200

    detect unsupported speed test, bump ODD to v2.4.4.3

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {}
 
 module.exports.GitHubReleasesUrl = `https://api.github.com/repos/KoalaBear84/OpenDirectoryDownloader/releases`
 module.exports.OpenDirectoryDownloaderVersion = {
-  version: `v2.4.4.1`,
-  releaseId: `74008221`
+  version: `v2.4.4.3`,
+  releaseId: `74282033`
 }
 module.exports.OpenDirectoryDownloaderFolder = `${__dirname}/ODD`
 module.exports.OpenDirectoryDownloaderOutputFolderName = `Scans`
